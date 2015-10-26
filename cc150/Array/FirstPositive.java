cd71b0a5f871adc2eaeda349456f65711430ed12
thomas <toncheng15@hotmail.com>
2015-10-26 00:41:44 -0700

    -first missing positive

--- cc150/Array/FirstPositive.java
+++ cc150/Array/FirstPositive.java
@@ -1,43 +1,32 @@
-public class FirstPositive {
-    /**    
-     * @param A: an array of integers
-     * @return: an integer
-     */
-    //note: put every positive integer i on index[i-1], then scan the array, find the first missing positive.
-      public int firstMissingPositive(int[] A) {
-        if(A == null || A.length == 0){
-            return 1;
+public class FirstMissingPositive {
+    public int firstMissingPositive(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
         }
-        int len = A.length;
-        for(int i =0; i< len; i++){
-         int temp = 0;
-            //watch out the order
-            while(A[i] != i + 1 && A[i] > 0 && A[i] <len + 1&& A[i] != A[A[i] - 1]){
-                temp = A[i];
-                A[i] = A[temp - 1];
-                A[temp - 1] = temp;
+        int len = nums.length;
+        int i = 0;
+        while(i < len){
+            // if the A[i] == i + 1, or A[i] out of range, jump to next
+            if(nums[i] <= 0 || nums[i] >= len || nums[i] == i + 1){
+                i++;
+            }
+            // deal with duplicates ( nums[i] != i + 1 cant handle dups)
+            else if(nums[nums[i] - 1] != nums[i]){
+                swap(nums, i, nums[i] - 1);
+            }else{
+                i++;
             }
         }
-
-        for(int j =0; j < len; j++){
-            if(A[j] != j + 1){
-                return j + 1;
-            }
+        i = 0;
+        while(i < len && nums[i] == i + 1){
+            i++;
         }
-        //no missing positive i in between
-        return len + 1;
+        return i + 1;
     }
-
-    public void swap(int a, int b){
-        int temp = b;
-        b = a;
-        a = temp;
-    }
-}
-
     
-    public static void main(String[] args){
-    	int [] A={2,1};
-    	System.out.print(firstMissingPositive(A));
+    private void swap(int[] nums, int a, int b){
+        int temp = nums[b];
+        nums[b] = nums[a];
+        nums[a] = temp;
     }
 }
