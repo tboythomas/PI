a6fa61a9331c510f84dcf7f9735f6d5f2f55fa19
thomas <toncheng15@hotmail.com>
2015-10-30 00:27:54 -0700

    -add method for sorted array

--- cc150/Array/Twosum.java
+++ cc150/Array/Twosum.java
@@ -26,4 +26,25 @@ public class 2sum {
         }
         return counter;
     }
-}
+
+    // if the input array is sorted
+    public int[] twoSum(int[] nums, int target){
+        if(nums == null || nums.length == 0){
+            return nums;
+        }
+        int[] res = new int[2];
+        int left = 0;
+        int right = nums.length - 1;
+        while(left < right){
+            int sum = nums[left] + nums[right];
+            if(sum > target){
+                right--;
+            }else if(sum < target){
+                left++;
+            }else{ // index based from 1
+                res[0] = left + 1;
+                res[1] = right + 1;
+            }
+        }
+        return res;
+    }
