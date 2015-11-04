08d55321a4d76becc4ed40358070b1f2b8ab28af
thomas <toncheng15@hotmail.com>
2015-11-04 00:08:43 -0800

    -fix tle

--- cc150/Tree/BSTserialization.java
+++ cc150/Tree/BSTserialization.java
@@ -1,53 +1,36 @@
-class BSTserialization {
+public class Codec {
 
-
-// note: 
-//   serialize: generate a string in preorder, (null node will be makred as -1), 
-//   use "." to separate each node.val
-//   deserialize: use a counter to keep track of the process, genreate the tree in preorder.
-class Solution {
-    /**
-     * This method will be invoked first, you should design your own algorithm 
-     * to serialize a binary tree which denote by a root node to a string which
-     * can be easily deserialized by your own "deserialize" method later.
-     */
-    public int counter = 0;
+    // Encodes a tree to a single string.
     public String serialize(TreeNode root) {
-        if(root == null){
-            return "";
-        }
-        String result = "";
-        result = maketree(root, result);
-        return result;
+        String res = "";
+        return buildhelper(root, res);
     }
     
-    public String maketree(TreeNode root, String result){
+    private String buildhelper(TreeNode root, String res){
         if(root == null){
-           return result+ "-1"+".";
+            return res + "X" + ",";
         }
-        result += root.val +".";
-        result = maketree(root.left, result);
-        result = maketree(root.right,result);
-        return result;
+        res += root.val + ",";
+        res = buildhelper(root.left, res);
+        res = buildhelper(root.right, res);
+        return res;
     }
     
+    // Decodes your encoded data to tree.
     public TreeNode deserialize(String data) {
-        if(data == null || data.length() == 0){
-            return null;
-        }
-        // splite the string by separator(".")
-        String[] preorder = data.split("\\.");
-        //generate null node
-        if(preorder[counter].equals("-1")){
-            counter++;
-            return null;
-        }
-
-        int val = Integer.parseInt(preorder[counter]);
-        TreeNode node = new TreeNode(val);
-        counter++;
-        node.left = deserialize(data);
-        node.right = deserialize(data);
-        return node;
+        Queue<String> q = new LinkedList<String>();
+        q.addAll(Arrays.asList(data.split(",")));
+        return treehelper(q);
+    }
+    
+    private TreeNode treehelper(Queue<String> q){
+        String cur = q.remove();
+        if(cur.equals("X")) return null;
+        
+            TreeNode root = new TreeNode(Integer.parseInt(cur));
+            root.left = treehelper(q);
+            root.right = treehelper(q);
+            return root;
+        
     }
 }
